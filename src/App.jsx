448e213dac36fa3643917c69ee3a0bdd839e783c
paullarasales <ahmadzufukli@gmail.com>
2024-11-03 22:08:23 +0800

    done passing data using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import './index.css';
-import Todo from './Todo.jsx';
+import Todo from './components/Todo.jsx';
 
 function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
       <ul 
         role="list"
         clasName="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-          <Todo />
+          <Todo name="Learn React" />
+          <Todo name="Build React" />
       </ul>
     </div>
   );
