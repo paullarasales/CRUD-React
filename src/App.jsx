092acd289a07458b8aa19ec19b16db64ea8a2d59
paullarasales <ahmadzufukli@gmail.com>
2024-11-03 20:52:41 +0800

    clean project for react crud practice

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css';
+
+function App(props) {
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <ul 
+        role="list"
+        clasName="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+          
+      </ul>
+    </div>
+  );
+}
+
+export default App;
